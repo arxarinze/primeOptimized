76355103d53296f51cd1a74ccac97fdf6fa005f3
Viktor Arinze Chinemelum <rxarinze@live.com>
2021-06-15 03:24:16 +0100

    added extra packages

--- src/controllers/prime.controller.ts
+++
@@ -1,7 +0,0 @@
-class PrimeController{
-    static getLargestNumberLessThanInput = (request: any, response: any)=>{
-        console.log('sfdsfd');
-    }
-}
-
-export default PrimeController
--- src/routes/prime.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import PrimeController from '../controllers/prime.controller';
-
-var router = express.Router();
-
-router.post(`/`, PrimeController.getLargestNumberLessThanInput);
-
-export default router
