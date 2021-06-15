2c02659114dc78686e0424d23197e6f0caa029c7
Viktor Arinze Chinemelum <rxarinze@live.com>
2021-06-15 03:26:13 +0100

    added middleware for input validation

---
+++ src/middleware/validate.middleware.ts
@@ -0,0 +1,24 @@
+export const requireJsonContent = () => {
+    return (request: any, response: any, next: any) => {
+        if (request.headers['content-type'] !== 'application/json') {
+            response.status(400).send({
+                status: 400, message: 'Server requires application/json'
+            })
+        } else {
+            next()
+        }
+    }
+}
+
+export const requireNumberGreaterThanOne = () => {
+    return (request: any, response: any, next: any)=>{let number = request.body.number ?? 0;
+    if (number <= 1) {
+        response.status(400).send({
+            status: 400,
+            message: "invalid input number should be > 0"
+        })
+    }
+    else{
+        next()
+    }}
+}
